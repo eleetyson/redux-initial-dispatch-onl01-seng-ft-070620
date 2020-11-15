239464a6f7aefef3851171daa01526e44c54c11d
Ethan <eleetyson@gmail.com>
2020-11-15 09:44:32 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,19 +1,24 @@
-let state = {count: 0};
+let state; // declare but don't assign state
 
-function changeState(state, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+// only need a default argument to set initial state
+function changeState(state={count: 0}, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state
   }
-
-function dispatch(action){
-    state = changeState(state, action)
-    render()
 }
 
-function render(){
-    document.body.textContent = state.count
+function dispatch(action) {
+  state = changeState(state, action)
+  render()
 }
+
+function render() {
+  document.body.textContent = state.count
+}
+
+// dispatching actions of '@@INIT' and 'INCREASE_COUNT'
+dispatch({ type: '@@INIT' }) //=> { count: 0 }
+dispatch({ type: 'INCREASE_COUNT' }) //=> { count: 1 }
